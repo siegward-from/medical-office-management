8816a976d19871f898332d60cffc255c2c0f3123
Renkas Vitalii <you@example.com>
2024-12-19 14:13:52 +0100

    Initialize rendez vous service

---
+++ rendezvous-service/src/main/java/com/example/rendezvous_service/RendezvousServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rendezvous_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendezvousServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendezvousServiceApplication.class, args);
+	}
+
+}
